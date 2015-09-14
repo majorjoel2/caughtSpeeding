06d078e5828ea51eefbe12d9553d90dac84b95e1
majorjoel2 <joel@tanner-family.org>
2015-09-14 08:34:57 -0400

    Done

--- caughtSpeeding/src/caughtSpeeding/Speeding.java
+++ caughtSpeeding/src/caughtSpeeding/Speeding.java
@@ -11,6 +11,29 @@ public class Speeding {
 	
 	public static int caughtSpeeding(int speed, boolean isBirthday)
 	{
-		return 0;
+		if (isBirthday == false) {
+			if (speed <= 60) {
+				return 0;
+			}
+			if (speed >= 61 && speed <= 80) {
+				return 1;
+			}
+			if (speed >= 81) {
+				return 2;
+			}
+		}
+		if (isBirthday == true) {
+			if (speed <= 65) {
+				return 0;
+			}
+			if (speed >= 66 && speed <= 85) {
+				return 1;
+			}
+			if (speed >= 86) {
+				return 2;
+			}
+		}
+		//this is for errors
+		return 0000000000000000000000;
 	}
 }
